e388c45d80c3e99c3d1990c39bbf39fe8d3e4b2e
patribezek <patribezek@gmail.com>
2017-02-01 16:27:41 -0300

    Add files via upload

--- Landing-form/js/custom.js
+++ Landing-form/js/custom.js
@@ -1,33 +1,58 @@
 
-
-
-
-function allowDrop(ev) {
-	ev.preventDefault();
-}
-
-function drag(ev) {
-	ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text");
-	ev.target.appendChild(document.getElementById(data));
-}
-
-
-var numProd = 1;
-
 $(document).ready(function(){
-	$(".agregar").click(function(){
-		$(".toClone").first().clone().appendTo(".cloned");
-		numProd++;
-		$(".num").append("<h4>Producto Nº: " + numProd + "</h4>");
 
-		/*if ((".num").hasChild == true) {
-			$(".num").remove("<h4>Producto Nº: " + numProd + "</h4>");
-		}*/
+
+	function allowDrop(ev) {
+		ev.preventDefault();
+	}
+
+	function drag(ev) {
+		ev.dataTransfer.setData("text", ev.target.id);
+	}
+
+	function drop(ev) {
+		ev.preventDefault();
+		var data = ev.dataTransfer.getData("text");
+		ev.target.appendChild(document.getElementById(data));
+	}
+
+
+	var numProd = 1;
+
+
+	$(".agregarSlide").click(function(){
+		$(".toCloneSlide").first().clone().appendTo(".clonedSlide");
+return false;
+		/*numProd++;
+		var classNum = "nombre"+numProd;
+
+		$(".num").append("<h4 class="+classNum+">Producto Nº: " + numProd + "</h4>");*/
 
 	});
+
+
+	$(".agregarProd").click(function(){
+		$(".toClone").first().clone().appendTo(".cloned");
+
+		numProd++;
+		var classNum = "nombre"+numProd;
+
+		$(".num").append("<h4 class="+classNum+">Producto Nº: " + numProd + "</h4>");
+
+	});
+
+
+
+
+
+
+
+
+
 });
+
+
+
+
+
+
