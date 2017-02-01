5b158c0975eef852f7343adab1b2fa0b8b940f34
patribezek <patribezek@gmail.com>
2017-02-01 15:09:43 -0300

    Add files via upload

--- Landing-form/js/custom.js
+++ Landing-form/js/custom.js
@@ -3,27 +3,31 @@
 
 
 function allowDrop(ev) {
-    ev.preventDefault();
+	ev.preventDefault();
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+	ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(data));
 }
 
 
-	var numProd = 1;
-
+var numProd = 1;
 
 $(document).ready(function(){
-    $(".agregar").click(function(){
-        $(".toClone").clone().appendTo(".cloned");
-        numProd++;
-        $(".well").append("<p>Producto Nº:" + numProd + "</p>");
-    });
+	$(".agregar").click(function(){
+		$(".toClone").first().clone().appendTo(".cloned");
+		numProd++;
+		$(".num").append("<h4>Producto Nº: " + numProd + "</h4>");
+
+		/*if ((".num").hasChild == true) {
+			$(".num").remove("<h4>Producto Nº: " + numProd + "</h4>");
+		}*/
+
+	});
 });
