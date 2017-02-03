5ec8a3991a2bc5ec0c77a1557c043aa0cce6efee
patribezek <patribezek@gmail.com>
2017-02-03 12:44:20 -0300

    fixes

--- Landing-form/js/custom.js
+++ Landing-form/js/custom.js
@@ -2,20 +2,20 @@
 
 
 
-	function allowDrop(ev) {
-		ev.preventDefault();
-	}
+function allowDrop(ev) {
+	ev.preventDefault();
+}
 
-	function drag(ev) {
-		ev.dataTransfer.setData("text", ev.target.id);
-	}
+function drag(ev) {
+	ev.dataTransfer.setData("text", ev.target.id);
+}
 
-	function drop(ev) {
-		ev.preventDefault();
-		var data = ev.dataTransfer.getData("text");
-		ev.target.appendChild(document.getElementById(data));
-	}
-	
+function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(data));
+}
+
 $(document).ready(function(){
 
 	var numProd = 1;
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
 	$(".agregarSlide").click(function(){
 		$(".toCloneSlide").first().clone().appendTo(".clonedSlide");
-return false;
+		return false;
 		/*numProd++;
 		var classNum = "nombre"+numProd;
 
@@ -34,12 +34,12 @@ return false;
 
 	$(".agregarProd").click(function(){
 		$(".toClone").first().clone().appendTo(".cloned");
-
+		/*
 		numProd++;
 		var classNum = "nombre"+numProd;
 
 		$(".num").append("<h4 class="+classNum+">Producto Nº: " + numProd + "</h4>");
-
+		*/
 	});
 
 
