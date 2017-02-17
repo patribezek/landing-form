e9e6bae08f09bf75765583aed07cf0577d717e40
patribezek <patribezek@gmail.com>
2017-02-17 16:48:06 -0300

    Add files via upload

---
+++ landing-form/js/custom.js
@@ -0,0 +1,60 @@
+
+
+
+
+function allowDrop(ev) {
+	ev.preventDefault();
+}
+
+function drag(ev) {
+	ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(data));
+}
+
+$(document).ready(function(){
+
+	var numProd = 1;
+
+
+	$(".agregarSlide").click(function(){
+		$(".toCloneSlide").first().clone().appendTo(".clonedSlide");
+		return false;
+		/*numProd++;
+		var classNum = "nombre"+numProd;
+
+		$(".num").append("<h4 class="+classNum+">Producto Nº: " + numProd + "</h4>");*/
+
+	});
+
+
+	$(".agregarProd").click(function(){
+		$(".toClone").first().clone().appendTo(".cloned");
+		/*
+		numProd++;
+		var classNum = "nombre"+numProd;
+
+		$(".num").append("<h4 class="+classNum+">Producto Nº: " + numProd + "</h4>");
+		*/
+	});
+
+
+$("form[name=asd]").bind('submit',function(){
+   console.log($('#my').serializeJSON());
+   return false;
+});
+
+
+});
+
+
+
+
+
+
+
+
